39ef032c60ef74d00178484a8fcad726e1fd700b
ben <ben@parelastic.com>
2014-05-21 15:58:03 -0400

    Enable license marking on various sql files.  Fix testware to handle comments.

--- tesora-dve-core/src/test/resources/com/tesora/dve/upgrade/catalog_version36.sql
+++ tesora-dve-core/src/test/resources/com/tesora/dve/upgrade/catalog_version36.sql
@@ -1,3 +1,23 @@
+---
+-- #%L
+-- Tesora Inc.
+-- Database Virtualization Engine
+-- %%
+-- Copyright (C) 2011 - 2014 Tesora Inc.
+-- %%
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License, version 3,
+-- as published by the Free Software Foundation.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU Affero General Public License for more details.
+-- 
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.
+-- #L%
+---
 create table auto_incr (incr_id integer not null auto_increment, nextId bigint not null, table_id integer, scope_id integer, primary key (incr_id)) ENGINE=InnoDB;
 create table character_sets (id integer(11) not null, character_set_name varchar(32) default '' not null, default_collate_name varchar(32) default '' not null, description varchar(60) default '' not null, maxlen bigint(3) default '0' not null, pe_character_set_name varchar(32) default '' not null, primary key (id)) ENGINE=InnoDB;
 create table config (config_id integer not null auto_increment, name varchar(255), value varchar(255), primary key (config_id)) ENGINE=InnoDB;
